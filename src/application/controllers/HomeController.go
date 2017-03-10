c80f914447f6d1635b6c7b5f57a64c0f37b2908f
Bjørn-Inge <bimorstad@gmail.com>
2017-03-10 08:42:03 +0100

    Visning av titel og antall spørsmål på forside

--- src/application/controllers/HomeController.go
+++ src/application/controllers/HomeController.go
@@ -8,12 +8,17 @@ import (
 type HomeController struct {
 }
 
+type User struct {
+	Title string
+	Email string
+}
+
 func (*HomeController) Index(res http.ResponseWriter, req *http.Request) {
 	// renderView(res, req, nil)
 	ut := models.UserTest{}
 	usertests := ut.FindAll()
 
-	createTemplate("Index", "home", "layout", usertests[0], res, req)
+	createTemplate("Index", "home", "layout", usertests, res, req)
 }
 
 func (HomeController) Create(res http.ResponseWriter, req *http.Request) {
